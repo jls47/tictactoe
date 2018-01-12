7377cb052d160617cf3358c3349709d098510345
John Luke Schaefer <lukeschaefer@gmail.com>
2018-01-11 21:52:21 -0800

    basic board functionality

---
+++ main.js
@@ -0,0 +1,39 @@
+var game = function(){
+    var turn = 1;
+    $(".box").click(function(){
+        var id = $(this).attr('id');
+        if($(this).is(':empty')){    
+            if(turn % 2 != 0){
+                $('#'+id).prepend("<img src='x.png'/>")
+                turn += 1;
+                var oldx = localStorage.getItem("xspaces");
+                if( oldx === null){
+                    localStorage.setItem("xspaces", id);
+                }else{
+                    localStorage.setItem("xspaces", [oldx + ", " + id]);
+                };
+                console.log(localStorage.getItem("xspaces"));
+            }else{
+                $('#'+id).prepend("<img src='o.png'/>")
+                turn += 1;
+                var oldo = localStorage.getItem("ospaces")
+                if( oldo === null){
+                    localStorage.setItem("ospaces", id);
+                }else{
+                    localStorage.setItem("ospaces", [oldo + ", " + id]);
+                };
+                console.log(localStorage.getItem("ospaces"));
+            };
+        }else{
+            alert("Choose another square!")
+        }
+    })
+    
+}
+
+$(".clear").click(function(){
+    localStorage.clear();
+    location.reload();
+})
+
+game();
